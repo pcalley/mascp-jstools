52c69269aced8218ed0b11fbde372a2d2cc09aec
Hiren Joshi <hirenj@gmail.com>
2011-10-17 22:18:24 -0700

    Support for clicking on and off individual snps in the controller to see which accessions they occurred in

--- dist/js/maschup.renderers.js
+++ dist/js/maschup.renderers.js
@@ -2769,6 +2769,7 @@ var addAnnotationToLayer = function(layerName,width,opts) {
     }
     
     this._renderer.redrawAnnotations(layerName,height);
+    return blob;
 };
 
 MASCP.CondensedSequenceRenderer.prototype._extendElement = function(el) {
